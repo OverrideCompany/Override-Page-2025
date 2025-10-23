043d75caa4a882e4a20029d7aff26b1ab6babee8
Christopher Alejandro Maldonado Chavez <christopher.cop787@gmail.com>
2025-10-23 04:50:45 +0000

    sigue igual de desfasado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased bg-background text-foreground flex flex-col min-h-dvh md:pb-0 pb-20">
+      <body className="font-body antialiased bg-background text-foreground flex flex-col min-h-dvh md:pb-0 pb-20 overflow-x-hidden">
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
             <ScrollProgressProvider>
                 <FloatingShapes />

93f5b38d0cbf1a547960daed8e39f62018271dea
Christopher Alejandro Maldonado Chavez <christopher.cop787@gmail.com>
2025-10-23 02:54:59 +0000

    tecnologias: Ktor, Python, Docker, Stack3 (_for element div_)

--- src/data/projects-data.ts
+++ src/data/projects-data.ts
@@ -16,7 +16,7 @@ export const projectsData: Project[] = [
       description: 'Override Pass redefine el control de accesos. Es un sistema basado en una aplicación móvil tan sencilla que la única tarea del usuario es iniciar sesión. Al instante, obtiene un código QR dinámico que funciona como su llave de acceso.\n\nEl corazón de nuestra plataforma es el encriptado STACK3. Este sistema propietario actualiza el código QR cada segundo, volviéndolo tan seguro que para vulnerarlo, se necesitaría viajar en el tiempo. Es seguridad de nivel superior, diseñada para el mundo real.\n\nSu eficiencia es inigualable: Override Pass funciona en la mayoría de los escenarios sin necesidad de una conexión a internet activa.\n\nEstá hecho para cualquier área: escuelas, empresas, eventos masivos o complejos residenciales. Donde sea que se necesite controlar un acceso, Override Pass es la solución ideal.\n\nSencillez, Rapidez y Seguridad.', 
       technologies: {
         frontend: ['Kotlin Multiplataforma', 'Compose Multiplataforma', 'Kotlin'],
-        backend: ['Node.js', 'Genkit AI', 'OAuth 2.0', 'Biometric Auth'],
+        backend: ['Ktor', 'Python', 'Docker', 'Stack3'],
         cloud: ['Firebase', 'Google Cloud']
       }, 
       imageUrl: 'https://ffvlhfaiswqbzayfivno.supabase.co/storage/v1/object/sign/Images/OverridePass.jpg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV82N2YzODg1Yy04ZDgyLTQyNzgtYjM4MC1iNGYwNWU4MGI5NTgiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJJbWFnZXMvT3ZlcnJpZGVQYXNzLmpwZyIsImlhdCI6MTc2MTE4MzcyMCwiZXhwIjoxNzkyNzE5NzIwfQ.UmCT-uNNdKwaDnveJFRhu5HncPTWXpqOpRlm-5rukJk', 
@@ -24,6 +24,7 @@ export const projectsData: Project[] = [
       color: '#8b5cf6' // Violeta
     },
   ];
+
 
 
 

61244a332a89e4d5385fb891e518eaf09f46cd21
Christopher Alejandro Maldonado Chavez <christopher.cop787@gmail.com>
2025-10-23 01:42:43 +0000

    cambia la imagen del fondo por esta https://ffvlhfaiswqbzayfivno.supabas

--- src/data/projects-data.ts
+++ src/data/projects-data.ts
@@ -15,8 +15,9 @@ export const projectsData: Project[] = [
       shortDescription: 'Tu llave maestra al ecosistema digital.',
       description: 'Override Pass es una solución de identidad digital unificada que ofrece acceso seguro y sin fricciones a toda nuestra suite de productos. Olvídate de múltiples contraseñas y gestiona tus servicios desde un solo lugar con la máxima seguridad.', 
       technologies: ['OAuth 2.0', 'Biometric Auth', 'Firebase', 'Next.js', 'Genkit AI'], 
-      imageUrl: PlaceHolderImages.find(p => p.id === 'override-pass')?.imageUrl!, 
+      imageUrl: 'https://ffvlhfaiswqbzayfivno.supabase.co/storage/v1/object/sign/Images/OverridePass.jpg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV82N2YzODg1Yy04ZDgyLTQyNzgtYjM4MC1iNGYwNWU4MGI5NTgiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJJbWFnZXMvT3ZlcnJpZGVQYXNzLmpwZyIsImlhdCI6MTc2MTE4MzcyMCwiZXhwIjoxNzkyNzE5NzIwfQ.UmCT-uNNdKwaDnveJFRhu5HncPTWXpqOpRlm-5rukJk', 
       imageHint: PlaceHolderImages.find(p => p.id === 'override-pass')?.imageHint!,
       color: '#8b5cf6' // Violeta
     },
   ];
+
